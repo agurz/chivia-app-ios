439ab3f0b519a163163b735fc64718f75e293c8a
Agustín Rodríguez <agurz@icloud.com>
2017-10-19 12:20:59 -0300

    Initial commit with mapbox directions api working as hell

---
+++ src/Chivia/ViewControllers/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Chivia
+//
+//  Created by Agustín Rodríguez on 10/18/17.
+//  Copyright © 2017 Agustín Rodríguez. All rights reserved.
+//
+
+import MapboxDirections
+import MapboxNavigation
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let origin = Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.9131752, longitude: -77.0324047), name: "Mapbox")
+        let destination = Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365), name: "White House")
+        
+        let options = RouteOptions(waypoints: [origin, destination])
+        options.routeShapeResolution = .full
+        options.includesSteps = true
+        
+        Directions.shared.calculate(options) { (waypoints, routes, error) in
+            guard let route = routes?.first else { return }
+            
+            let viewController = NavigationViewController(for: route)
+            self.present(viewController, animated: true, completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
